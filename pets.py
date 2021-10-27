934bc0d30f7395473dfc38d9fb4a79e4483c2bd7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-27 14:18:55 -0700

    create ninja and pets modules

--- pets.py
+++ pets.py
@@ -9,22 +9,31 @@ import utl
 
 #  **** Pet Class ******************************************
 class Pet:
-    # implement __init__( name , type , tricks ):
-    def __init__(self, name, type, tricks) -> None:
-        pass
+    # implement __init__( name="toto" , type="dog" , tricks=["roll over", "play dead"] ):
+    def __init__(self, name="toto" , type="dog" , tricks=["roll over", "play dead"]) -> None:
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = 50
+        self.energy = 50
 
-    # sleep() - increases the pets energy by 25
+    # sleep() - increases the pets energy by 25
     def sleep(self):
         pass
 
-    # eat() - increases the pet's energy by 5 & health by 10
+    # eat() - increases the pet's energy by 5 & health by 10
     def ear(self):
         pass
 
-    # play() - increases the pet's health by 5
+    # play() - increases the pet's health by 5
     def play(self):
         pass
 
-    # noise() - prints out the pet's sound
+    # noise() - prints out the pet's sound
     def noise(self):
         pass
+
+    # print pet stats
+    def info(self):
+        utl.print_desc_center("Pet Info")
+        print(F"Pet Name: {self.name} :: Type : {self.type} :: Tricks : {self.tricks} :: Health : {self.health} :: Energy : {self.energy}")
--- utl.py
+++ utl.py
@@ -4,7 +4,7 @@
 # //////////////////////////////////////////////////////////
 
 def print_desc(description:str):
-    print(f"{f'**** {description} ':*<100}\n")
+    print(f"{f'---- {description} ':-<100}")
 
 def print_desc_center(description:str):
-    print(f"{f'**** {description} ':*^100}\n")
+    print(f"{f' {description} ':-^100}")
